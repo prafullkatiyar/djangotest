7002a1fcf4e1e89314a273c72b78ab6784369266
prafull <prafull@takatarecovery.com>
2020-06-19 09:40:25 +1000

    3

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from . import urls
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    context = {}
+    return render(request, 'polls/index.html', context)
+
