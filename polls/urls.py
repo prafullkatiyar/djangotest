fba79304ca1875efccdce0bdee4f60df20ece888
prafull <prafull@takatarecovery.com>
2020-06-23 09:16:58 +1000

    6

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index-url-original'),
+    path('', views.index, name='index-url-original-reworked'),
 ]
