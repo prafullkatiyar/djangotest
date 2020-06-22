2882cc025637ad55a309d763b4b1e3741d8ddbc0
prafull <prafull@takatarecovery.com>
2020-06-23 09:12:55 +1000

    5

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index-url-polls-app-changed'),
+    path('', views.index, name='index-url-original'),
 ]
